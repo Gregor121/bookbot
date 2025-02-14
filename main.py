ae24dbea3d6887afad5f0866943fc1bb425e0595
Gregor121 <greg.brown121@gmail.com>
2025-02-13 21:12:38 -0700

    Function 2

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
+    
+
 def word_count(string):
     count = len(string.split())
     print(count)
 
-word_count(file_contents)
+def string_count(string):
+    strings = {}
+    file_contents = string.lower()
+    for character in file_contents:
+        if character in strings.keys():
+            strings[character] += 1
+        else:
+            strings.update({character: 1})
+    print(strings)
+
+string_count(file_contents)
+
